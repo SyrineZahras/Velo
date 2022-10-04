fac8529f02a6275b037915d05655c470f2569587
SyrineZahras <syrine.zahras@esprit.tn>
2022-10-04 12:13:11 +0100

    template integration

---
+++ resources/views/FrontEnd/Footer.blade.php
@@ -0,0 +1,17 @@
+<section id="footer">
+            <div class="footer-section">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <p class="footer-text" >Copyright <span class="copyright"> &copy;</span>2017</p>
+                        </div>
+                        <div class="col-md-4">
+                            <p class="footer-text">Biking Theme</p>
+                        </div>
+                        <div class="col-md-4">
+                            <p class="footer-text">Brought to you by <span class="usama"><a href="https://www.facebook.com/UsamaElsayed90">Usama Elsayed</a></span></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
